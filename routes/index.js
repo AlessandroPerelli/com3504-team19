343c9d1f255a2f829e1fac55f3ba9a1f598f1cab
Alessandro-Perelli-University <aperelli1@sheffield.ac.uk>
2024-03-09 14:29:20 +0000

    added container to center content

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'login' });
+  res.render('login', { title: 'Welcome to Plant Viewer' });
 });
 
 module.exports = router;
