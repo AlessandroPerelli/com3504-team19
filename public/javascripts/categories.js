83de9d49bc5b385f4f8ec7f1a373be3b7e27cf28
Alessandro-Perelli-University <aperelli1@sheffield.ac.uk>
2024-03-12 13:04:06 +0000

    created categories javascript file to hold categories list

---
+++ public/javascripts/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+  {
+    categoryName: "Popular",
+    plants: [
+      { name: "Peashooter", image: "/images/peashooter.png" },
+      { name: "Sunflower", image: "/images/sunflower.jpg" },
+    ],
+  },
+  {
+    categoryName: 'New Arrivals',
+    plants: [
+      { name: 'Big Leaf', image: '/images/big.jpg' },
+      { name: 'Marigold', image: '/images/marigold.jpg' },
+    ],
+  },
+];
