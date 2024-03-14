d2f0d343dd1dfa3018bd23fc7886c1fb17fb872f
AlessandroPerelli <alessandroperelli156@gmail.com>
2024-03-14 23:05:42 +0000

    fixed error when reading in image file and view

--- public/javascripts/imageUpload.js
+++ public/javascripts/imageUpload.js
@@ -1,10 +1,11 @@
-const dropArea = document.getElementById("image-drop-area");
-const imageFile = document.getElementById("image-file");
-const imageView = document.getElementById("image-preview");
+const dropArea = document.getElementsByClassName("image-drop-area")[0];
+const imageFile = document.getElementsByClassName("image-file")[0];
+const imageView = document.getElementsByClassName("image-preview")[0];
 
 imageFile.addEventListener("change", uploadImage);
 
 function uploadImage() {
+  console.log("upload image");
   let imgLink = URL.createObjectURL(imageFile.files[0]);
   imageView.style.backgroundImage = `url(${imgLink})`;
   imageView.textContent = "";
