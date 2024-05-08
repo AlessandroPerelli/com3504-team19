c8eb3482c8719e5e5175a4dd4d464846b1a546dd
Hengjohmnan Phoeuk <hengjohmnanphoeuk@Hengjohmnans-MacBook-Air.local>
2024-05-08 22:16:17 +0100

    Plant characteristics and fixed image storing

--- controllers/plants.js
+++ controllers/plants.js
@@ -9,29 +9,42 @@ exports.create = function (userData, filePath) {
     name: userData.name,
     time: userData.time,
     location: userData.location,
-    size: userData.size,
     desc: userData.desc,
-    categories: userData.categories,
     img: filePath,
+
+    sight_time: userData.sight_time,
+    height: userData.height,
+    spread: userData.spread,
+
+    //Plant Characteristics
+    does_the_plant_have_flowers: userData.does_the_plant_have_flowers === 'on',
+    does_the_plant_have_leaves: userData.does_the_plant_have_leaves === 'on',
+    does_the_plant_have_fruit: userData.does_the_plant_have_fruit === 'on',
+    full_sun: userData.full_sun === 'on',
+    partial_shade: userData.partial_shade === 'on',
+    full_shade: userData.full_shade === 'on',
+    evergreen: userData.evergreen === 'on',
+    succulent: userData.succulent === 'on',
+
+    identification_status: userData.identification_status,
+
+    user_nickname: userData.user_nickname,
   });
 
   // Save the plant to the database and handle success or failure
-  return plant
-    .save()
-    .then((plant) => {
-      // Log the created plant
-      console.log(plant);
+  return plant.save().then(plant => {
+    // Log the created plant
+    console.log(plant);
 
-      // Return the plant data as a JSON string
-      return JSON.stringify(plant);
-    })
-    .catch((err) => {
-      // Log the error if saving fails
-      console.log(err);
+    // Return the plant data as a JSON string
+    return JSON.stringify(plant);
+  }).catch(err => {
+    // Log the error if saving fails
+    console.log(err);
 
-      // Return null in case of an error
-      return null;
-    });
+    // Return null in case of an error
+    return null;
+  });
 };
 
 // Function to get all students
